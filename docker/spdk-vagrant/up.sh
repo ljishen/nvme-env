ec288723b5bac8397663b8229f96590ce3d1488a
Jianshen Liu <jliu120@ucsc.edu>
2017-07-02 20:00:05 -0700

    Change to the vagrant dir in Dockerfile

--- docker/spdk-vagrant/up.sh
+++ docker/spdk-vagrant/up.sh
@@ -2,8 +2,7 @@
 
 set -e
 
-cd spdk/scripts/vagrant && \
-        source ./env.sh && \
-        vagrant up && \
-        vagrant ssh && \
-	/bin/bash # add this so that typing `exit` can exit the VM and go back to the docker container
+source ./env.sh && \
+    vagrant up && \
+    vagrant ssh && \
+    /bin/bash # add this so that typing `exit` can exit the VM and go back to the docker container
