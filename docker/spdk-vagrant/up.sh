b458114e0b91990fb202eccc335a5354f50808fc
Jianshen Liu <jliu120@ucsc.edu>
2017-07-07 23:41:21 -0700

    Use env variable to prevent the bash to get started in a loop

--- docker/spdk-vagrant/up.sh
+++ docker/spdk-vagrant/up.sh
@@ -4,5 +4,10 @@ set -e
 
 source ./env.sh && \
     vagrant up && \
-    vagrant ssh && \
+    vagrant ssh
+
+# Use this to guard so that the bash env won't get started again and again
+if [ -z "$STARTED" ]; then
+    export STARTED=1
     /bin/bash # add this so that typing `exit` can exit the VM and go back to the docker container
+fi
